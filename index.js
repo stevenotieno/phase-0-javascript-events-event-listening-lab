4769fce1e0292d8e7f549ef84c931b1a4145ae25
+ <+>
2022-12-09 14:09:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
 }
+const input = document.querySelector('input');
+
+// Binds an event listener in addingEventListener()
+function addingEventListener() {
+
+	input.addEventListener('click', function(event) {
+		alert('I was clicked!');
+	});
+
+}
